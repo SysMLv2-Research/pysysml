8edd9a9c601c5eb404004662f1c707f5e01e4ae6
HansBug <hansbug@buaa.edu.cn>
2024-10-10 21:14:58 +0800

    dev(hansbug): add init test

--- pysysml/kerml/__init__.py
+++ pysysml/kerml/__init__.py
@@ -1,1 +1,5 @@
+import os.path
+
 from .base import list_reserved_words, is_reserved_word
+
+__grammar_file__ = os.path.normpath(os.path.join(__file__, '..', 'syntax.lark'))
---
+++ test/kerml/test_init.py
@@ -0,0 +1,17 @@
+import os.path
+
+import pytest
+
+
+@pytest.mark.unittest
+class TestKerMLInit:
+    def test_grammar_file(self):
+        from pysysml.kerml import __grammar_file__
+
+        assert os.path.exists(__grammar_file__), f'Grammar file not exist, {__grammar_file__!r}'
+        assert os.path.isfile(__grammar_file__), f'Grammar file not a file, {__grammar_file__!r}'
+
+        assert os.path.samefile(
+            __grammar_file__,
+            os.path.normpath(os.path.join('pysysml', 'kerml', 'syntax.lark'))
+        )
